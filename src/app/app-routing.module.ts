48f43bde92dcc563fea33f91fee66bc225518506
KesavaSai <kesavasaiguntur@gmail.com>
2021-08-21 12:55:41 +0530

    commiting inventory angular project 1.0

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateProductComponent } from './create-product/create-product.component';
+import { DealersListComponent } from './dealers-list/dealers-list.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { RegisterDealerComponent } from './register-dealer/register-dealer.component';
+import { UpdateProductComponent } from './update-product/update-product.component';
 
-const routes: Routes = [];
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  {path:'products',component:ProductListComponent},
+  {path:'add',component:CreateProductComponent},
+  {path:'update/:id',component:UpdateProductComponent},
+  {path:'details/:id',component:ProductDetailsComponent},
+  {path:'dealers',component:DealersListComponent},
+  {path:'register',component:RegisterDealerComponent},
+  {path:'login',component:LoginComponent},
+  {path:'logout',component:LogoutComponent},
+  {path: '', redirectTo: 'home', pathMatch: 'full' },
+  {path:'home', component:HomeComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
